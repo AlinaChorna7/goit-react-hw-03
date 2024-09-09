da725924826385b0c0458343ed27d49237a8f279
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-09-09 14:20:21 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
-import  {useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './components/ContactForm/ContactForm';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
 
-
 export default function App() {
-  const [contacts, setContacts] = useState([
+  const initialContacts = JSON.parse(localStorage.getItem('contacts')) || [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ];
 
-const [filter, setFilter] = useState('')
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState('');
 
-  const handleDelete = (id) => {
-    setContacts(contacts.filter(contact => contact.id !== id));
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+ 
+  const handleAddContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
-const handleAddContact= (newContact)=>{
-  setContacts(prevContacts =>[...prevContacts, newContact])
-}
+  
+  const handleDelete = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
 
-const filteredContacts = contacts.filter(contact =>
-  contact.name.toLowerCase().includes(filter.toLowerCase())
-)
-
+ 
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
-    <div  className="container">
+    <div className="container">
       <h1>Phonebook</h1>
-      <ContactForm  onAddContact={handleAddContact}/>
-      <SearchBox  filter={filter} onFilterChange={setFilter}/>
-      {filteredContacts.length > 0 ? (
+      <ContactForm onAddContact={handleAddContact} />
+      <SearchBox filter={filter} onFilterChange={setFilter} />
       <ContactList contacts={filteredContacts} onDelete={handleDelete} />
-    ) : (
-      <p>No contacts found</p>  
-    )}    </div>
+    </div>
   );
 }
